772cae3762e13ee1fba894c723690a791b5f8ffc
Jonathan Gala <jon@jongala.com>
2017-03-30 00:07:47 -0400

    Rendering background, two shape foreground, noise. Simple palette.

--- js/shapescape.js
+++ js/shapescape.js
@@ -19,21 +19,68 @@
         }
     }
 
+    function addNoise(canvas, opacity) {
+        var ctx = canvas.getContext('2d'),
+        x,
+        y,
+        noise,
+        opacity = opacity || .2;
+
+        var w = canvas.width;
+        var h = canvas.height;
+
+        for ( x = 0; x < w; x++ ) {
+          for ( y = 0; y < h; y++ ) {
+             noise = Math.floor( Math.random() * 255 );
+
+             ctx.fillStyle = "rgba(" + noise + "," + noise + "," + noise + "," + opacity + ")";
+             ctx.fillRect(x, y, 1, 1);
+          }
+        }
+    }
+
+    function addNoisePattern(canvas, opacity, tileSize, useOverlay) {
+        // create an offscreen canvas where we will generate
+        // noise to use as a pattern
+        tileSize = tileSize || 100;
+        var noise = document.createElement('canvas');
+        noise.width = tileSize;
+        noise.height = tileSize;
+        var noiseCtx = noise.getContext('2d');
+
+        // main canvas context
+        var ctx = canvas.getContext('2d');
+
+        // add noise to the offscreen canvas
+        addNoise(noise, opacity);
+
+        // create a noise pattern from the offscreen canvas
+        var noisePattern = ctx.createPattern(noise, 'repeat');
+        ctx.fillStyle = noisePattern;
+        if (useOverlay) {
+            ctx.globalCompositeOperation = 'overlay';
+        }
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        if (useOverlay) {
+            ctx.globalCompositeOperation = 'normal';
+        }
+    }
+
     // random Array member
     function randItem(arr) {
         return arr[Math.floor(arr.length * Math.random())];
-    }
-
-    // Random palette member
-    function randomColor(PALETTE) {
-      return randItem(PALETTE);
     }
 
     function randomInRange(min, max) {
         return (min + (max - min) * Math.random());
     }
 
-    
+    var PALETTE = ['#222222', '#f7d4f8', '#b966d3', '#362599', '#fff9de', '#fae1f6'];
+
+    function getFill() {
+        return randItem(PALETTE);
+    }
+
 
     function setAttrs(el, attrs) {
         if (el && el.setAttribute) {
@@ -48,15 +95,53 @@
 
     function drawCircle(ctx, x, y, r, fill, stroke, alpha) {
         alpha = (alpha === undefined) ? 1 : alpha;
-        console.log('alpha', alpha);
         ctx.fillStyle = fill;
-        //ctx.strokeStyle = stroke;
+        ctx.strokeStyle = stroke;
         ctx.globalAlpha = alpha;
         ctx.beginPath();
-        ctx.moveTo(x, y);
+        ctx.moveTo(x + r, y);
         ctx.arc(x, y, r, 0, 2 * Math.PI, false);
         ctx.fill();
+        stroke && ctx.stroke();
         ctx.closePath();
+    }
+
+    function addCircle(ctx, w, h, opts) {
+        drawCircle(
+            ctx,
+            w/2,
+            h/2,
+            Math.min(w,h) * randomInRange(0.2, 0.4),
+            getFill()
+        );
+        return ctx;
+    }
+
+    function addTriangle(ctx, w, h, opts) {
+        var d = Math.min(w, h) * randomInRange(0.5, 0.85);
+        var cx = w/2;
+        var cy = randomInRange(h/3, 2 * h/3);
+        var leg = Math.cos(30 * Math.PI/180) * (d / 2);
+        ctx.fillStyle = getFill();
+        ctx.beginPath();
+        ctx.moveTo(cx, cy - leg);
+        ctx.lineTo(cx + d/2, cy + leg);
+        ctx.lineTo(cx - d/2, cy + leg);
+        ctx.closePath();
+        ctx.fill();
+        return ctx;
+    }
+
+    function addSquare(ctx, w, h, opts) {
+        var d = Math.min(w, h) * 0.5;
+        ctx.fillStyle = getFill();
+        ctx.fillRect(
+            w/2 - d/2,
+            randomInRange(h/3, 2 * h/3) - d/2,
+            d,
+            d
+        );
+        return ctx;
     }
 
 
@@ -115,21 +200,42 @@
             ctx.clearRect(0, 0, w, h);
         }
         
-        // RENDER HERE
+        var renderer;
+        var renderMap = {
+            'circle': addCircle,
+            'triangle': addTriangle,
+            'square': addSquare
+        };
+        var shapes = Object.keys(renderMap);
 
+        // BEGIN RENDERING
 
+        // add one or two bg blocks
+        ctx.fillStyle = getFill();
+        ctx.fillRect(0, 0, w, h);
+        if (Math.random() > 0.66) {
+            var horizon = randomInRange(h * 0.4, h * 0.85);
+            ctx.fillStyle = getFill();
+            ctx.fillRect(0, horizon, w, h - horizon);
+        }
 
-        drawCircle(
-            ctx,
-            w/2,
-            h/2,
-            Math.min(w,h) * 0.25,
-            'black'
-        );
+        // draw two shape layers in some order:
+        // shuffle shape list
+        shapes.sort(function(a, b) { return randomInRange(-1, 1)});
 
+        // pop a renderer name, get render func and execute X 2
+        renderMap[shapes.pop()](ctx, w, h, opts);
+        renderMap[shapes.pop()](ctx, w, h, opts);
         
 
+        // Add effect elements
+        // ...
+
+
+        // add noise
+        addNoisePattern(el, 0.04);
       
+        // END RENDERING
 
         // if new canvas child was created, append it
         if (newEl) {
