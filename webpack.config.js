19f290bd8b347f83b417e53e30a03a5e16eb69bb
Jonathan Gala <jon@jongala.com>
2018-06-06 18:06:23 -0400

    Move dist to project root for ease of GH pages and basic use

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin'); //  -> ADDED I
 
 // Constant with our paths
 const paths = {
-  DIST: path.resolve(__dirname, 'dist'),
+  DIST: path.resolve(__dirname, '.'),
   SRC: path.resolve(__dirname, 'src'),
   JS: path.resolve(__dirname, 'src/js'),
 };
