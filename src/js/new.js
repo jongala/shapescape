7d0c77e70e0dcf329c3fec6957593f019624bcc9
Jonathan Gala <jon@jongala.com>
2020-01-16 18:21:57 -0500

    Add template for new renderer in new.js

---
+++ src/js/new.js
@@ -0,0 +1,88 @@
+import noiseUtils from './noiseutils';
+import palettes from './palettes';
+import { randItem, randomInRange, resetTransform, rotateCanvas, getGradientFunction, getSolidColorFunction } from './utils';
+import { drawCircle, drawRing, drawTriangle, drawSquare, drawRect, drawBox, drawPentagon, drawHexagon } from './shapes';
+
+const DEFAULTS = {
+    container: 'body',
+    palette: palettes.ultra,
+    addNoise: 0.04,
+    noiseInput: null,
+    dust: false,
+    skew: 1, // normalized skew
+    clear: true,
+    lightMode: 'auto', // [auto, bloom, normal]
+    gridMode: 'auto', // [auto, coarse, fine]
+}
+
+const PI = Math.PI;
+
+// Main function
+export function newName(options) {
+    let opts = Object.assign({}, DEFAULTS, options);
+
+    let container = opts.container;
+    let cw = container.offsetWidth;
+    let ch = container.offsetHeight;
+    let SCALE = Math.min(cw, ch);
+
+    // Find or create canvas child
+    let el = container.querySelector('canvas');
+    let newEl = false;
+    if (!el) {
+        container.innerHTML = '';
+        el = document.createElement('canvas');
+        newEl = true;
+    }
+    if (newEl || opts.clear) {
+        el.width = cw;
+        el.height = ch;
+    }
+
+    let ctx = el.getContext('2d');
+
+
+    // Color funcs
+    // --------------------------------------
+
+    let getSolidFill = getSolidColorFunction(opts.palette);
+
+    // shared colors
+    let bg = getSolidFill();
+
+    // get palette of non-bg colors
+    let contrastPalette = [].concat(opts.palette);
+    contrastPalette.splice(opts.palette.indexOf(bg), 1);
+    let getContrastColor = getSolidColorFunction(contrastPalette);
+
+    // shared foregrounds
+    let fg = getContrastColor();
+
+
+    // Draw Stuff
+    // --------------------------------------
+
+
+
+
+    // Finish up
+    // --------------------------------------
+
+    // add noise
+    if (opts.addNoise) {
+        if (opts.noiseInput) {
+            // apply noise from supplied canvas
+            noiseUtils.applyNoiseCanvas(el, opts.noiseInput);
+        } else {
+            // create noise pattern and apply
+            noiseUtils.addNoiseFromPattern(el, opts.addNoise, w / 3);
+        }
+    }
+
+    // if new canvas child was created, append it
+    if (newEl) {
+        container.appendChild(el);
+    }
+}
+
+
