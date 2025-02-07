b28e90ae4352c92fd047e5b107b5f739fbe141fd
Jonathan Gala <jon@jongala.com>
2025-02-06 20:11:56 -0500

    moire: step thru bezier with transverse lines

--- src/js/moire.js
+++ src/js/moire.js
@@ -1,7 +1,7 @@
 import noiseUtils from './noiseutils';
 import palettes from './palettes';
 import { randItem, randomInRange, randomInt, resetTransform, rotateCanvas, getGradientFunction, getSolidColorFunction, getAngle, getVector, mapKeywordToVal } from './utils';
-import { drawCircle, drawRing, drawTriangle, drawSquare, drawRect, drawBox, drawPentagon, drawHexagon } from './shapes';
+import { drawCircle, drawRing, drawTriangle, drawSquare, drawRect, drawBox, drawPentagon, drawHexagon, drawCross } from './shapes';
 
 const DEFAULTS = {
     container: 'body',
@@ -134,10 +134,13 @@ export function moire(options) {
 
     ctx.strokeStyle = 'black';
     function stepCurve(a, b, c1, c2, steps) {
-        ctx.beginPath();
-        ctx.moveTo(...a);
-        ctx.bezierCurveTo(...c1, ...c2, ...b);
-        ctx.stroke();
+        // ctx.beginPath();
+        // ctx.moveTo(...a);
+        // ctx.bezierCurveTo(...c1, ...c2, ...b);
+        // ctx.stroke();
+
+        drawCross(ctx, ...c1, 10, {stroke:'blue'});
+        drawCross(ctx, ...c2, 10, {stroke:'green'});
 
         let t = 0;
         let inc = 1/steps;
@@ -147,19 +150,46 @@ export function moire(options) {
         let _x = x; // last x
         let _y = y; // last y
 
+        // each point on curve
+        //drawCircle(ctx, x, y, 10, {stroke:'red'});
+
         let v;
+        let p1 = [0, 0]; // start point of transverse line
+        let p2 = [0, 0]; // end point
+
+        let l = 100;
 
         // step thru the bezier
-        for (var i = 1; i < steps; i++) {
-            console.log(t);
+        for (var i = 1; i <= steps; i++) {
+            t = i * inc;
+
             x = (1 - t) ** 3 * a[0] +       3 * (1 - t) ** 2 * t * c1[0] +      3 * (1 - t) * t ** 2 * c2[0] +      t ** 3 * b[0];
             y = (1 - t) ** 3 * a[1] +       3 * (1 - t) ** 2 * t * c1[1] +      3 * (1 - t) * t ** 2 * c2[1] +      t ** 3 * b[1];
-            t += inc;
 
-            drawCircle(ctx, x, y, 100, {stroke:'green'});
-            console.log(x, y);
+            // get vector from last point to this point
+            v = getVector([_x, _y], [x, y]);
+
+            // draw trasnverse line
+            p1 = [
+                _x + l * Math.cos(v.angle - PI/2),
+                _y + l * Math.sin(v.angle - PI/2),
+            ];
+            p2 = [
+                _x + l * Math.cos(v.angle + PI/2),
+                _y + l * Math.sin(v.angle + PI/2),
+            ];
 
 
+            ctx.strokeStyle = fg;
+
+            ctx.beginPath();
+            ctx.moveTo(...p1);
+            ctx.lineTo(...p2);
+            ctx.stroke();
+
+            // update point
+            _x = x;
+            _y = y;
         }
     }
 
@@ -172,8 +202,11 @@ export function moire(options) {
     );
 
 
+    // same density
+    // different density
+    // same endpoints
+    // different endpoints
 
-    //console.log(lines);
 
 
     // Finish up
